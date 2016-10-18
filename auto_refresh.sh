3736f62ab6212ac62d5c410d6f86a1e04a50ee91
johnchower <johnchower@gmail.com>
2016-10-18 13:18:44 -0600

    Add output flag to auto_refresh.sh to remove unneccesary step of renaming output.

--- auto_refresh.sh
+++ auto_refresh.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 cd ~/Projects/notebook_tests
-jupyter-nbconvert --to notebook --execute Test_ggplot.ipynb
-mv Test_ggplot.nbconvert.ipynb Test_ggplot.ipynb
+jupyter-nbconvert --to notebook --execute Test_ggplot.ipynb --output Test_ggplot.ipynb
 git add Test_ggplot.ipynb
 git commit -m "Update Test_ggplot notebook $(date)"
 git push
