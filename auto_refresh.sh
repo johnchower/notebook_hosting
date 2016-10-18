fa32885baa51b7d8de2ec9af851324623f4e2b3a
johnchower <johnchower@gmail.com>
2016-10-18 13:10:46 -0600

    Ignore vim session file and add cd line to refresh script.

--- auto_refresh.sh
+++ auto_refresh.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+cd ~/Projects/notebook_tests
 jupyter-nbconvert --to notebook --execute Test_ggplot.ipynb
 mv Test_ggplot.nbconvert.ipynb Test_ggplot.ipynb
 git add Test_ggplot.ipynb
-git commit -m "Update notebook $(date)"
+git commit -m "Update Test_ggplot notebook $(date)"
 git push
